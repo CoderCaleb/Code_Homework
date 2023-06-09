8eaa051a72a5a42acabcd688ef94f9368dffdb1b
CoderCaleb <xxgoldenprozxx@gmail.com>
2023-06-09 21:59:43 +0800

    PRO-C97_SIMPLE-PYTHON-PROGRAMS

---
+++ convert.py
@@ -0,0 +1,7 @@
+height_cm = int(input("Enter the height in centimeters: "))
+
+inches = height_cm * 0.394
+feet = height_cm * 0.0328
+
+print("The length in inches:", round(inches, 2))
+print("The length in feet:", round(feet, 2))
