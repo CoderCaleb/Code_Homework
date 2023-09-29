200362d3e0a3c9665553b343587979a537752224
Caleb <xxgoldenprozxx@gmail.com>
2023-09-29 15:28:17 +0800

    Done PRO-C110 and PRO-C108

--- PRO-106/Walkers.py
+++ PRO-106/Walkers.py
@@ -1,31 +1,23 @@
 import cv2
 
-# Create our body classifier
 body_classifier = cv2.CascadeClassifier('/Users/caleb/Code_Homework-1/PRO-106-ProjectTemplate-main/haarcascade_fullbody.xml')
 
-# Initiate video capture for video file
 cap = cv2.VideoCapture('/Users/caleb/Code_Homework-1/PRO-106-ProjectTemplate-main/walking.avi')
 
-# Loop once video is successfully loaded
 while True:
     
-    # Read first frame
     ret, frame = cap.read()
 
-    # Convert Each Frame into Grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
-    # Pass frame to our body classifier
     bodies = body_classifier.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
 
-    # Extract bounding boxes for any bodies identified
     for (x, y, w, h) in bodies:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 255), 2)
 
-    # Display the frame with rectangles drawn around bodies
     cv2.imshow('Pedestrians', frame)
 
-    if cv2.waitKey(1) == 32: # 32 is the Space Key
+    if cv2.waitKey(1) == 32:
         break
 
 cap.release()
--- PRO-C107/obj_trk_final.py
+++ PRO-C107/obj_trk_final.py
@@ -9,16 +9,12 @@ xs = []
 ys = []
 
 video = cv2.VideoCapture("/Users/caleb/Code_Homework-1/project-C107-template-main/footvolleyball.mp4")
-# Load tracker
 tracker = cv2.TrackerCSRT_create()
 
-# Read the first frame of the video
 success, img = video.read()
 
-# Select the bounding box on the image
 bbox = cv2.selectROI("tracking", img, False)
 
-# Initialize the tracker on the img and the bounding box
 tracker.init(img, bbox)
 
 def goal_track(img, bbox):
@@ -46,27 +42,19 @@ def drawBox(img, bbox):
     cv2.putText(img, "Tracking", (75, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
 
 while True:
-    # Read the video and store the values in check and img variables
     success, img = video.read()
 
-    # Use the tracker.update() and store the values in the success and bbox variables
     success, bbox = tracker.update(img)
 
-    # Check if we are getting the updated image
     if success:
-        # Call drawBox() and pass img and bbox
         drawBox(img, bbox)
     else:
-        # Put the text on the screen saying “LOST”
         cv2.putText(img, "LOST", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
-    # Call the goal_track() function and pass img and bbox as parameters to it
     goal_track(img, bbox)
 
-    # Call the cv2.imshow() function to show the image
     cv2.imshow("hand tracking", img)
 
-    # Use the 'q' key to break the loop
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
